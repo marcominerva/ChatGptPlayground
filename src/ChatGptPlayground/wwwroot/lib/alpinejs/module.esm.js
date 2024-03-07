5d4401e4f1497a90fcbe3e081af70da0cd5c87c5
Marco Minerva <marco.minerva@gmail.com>
2024-03-07 14:36:06 +0100

    Update client libraries

--- src/ChatGptPlayground/wwwroot/lib/alpinejs/module.esm.js
+++ src/ChatGptPlayground/wwwroot/lib/alpinejs/module.esm.js
@@ -203,17 +203,27 @@ function initTree(el, walker = walk, intercept = () => {
 }) {
   deferHandlingDirectives(() => {
     walker(el, (el2, skip) => {
+      if (el2._x_inited) {
+        if (el2._x_ignore)
+          skip();
+        return;
+      }
       intercept(el2, skip);
       initInterceptors.forEach((i) => i(el2, skip));
       directives(el2, el2.attributes).forEach((handle) => handle());
-      el2._x_ignore && skip();
+      if (el2._x_ignore) {
+        skip();
+      } else {
+        el2._x_inited = true;
+      }
     });
   });
 }
-function destroyTree(root) {
-  walk(root, (el) => {
+function destroyTree(root, walker = walk) {
+  walker(root, (el) => {
     cleanupAttributes(el);
     cleanupElement(el);
+    delete el._x_inited;
   });
 }
 
@@ -358,8 +368,6 @@ function onMutate(mutations) {
     node._x_ignore = true;
   });
   for (let node of addedNodes) {
-    if (removedNodes.has(node))
-      continue;
     if (!node.isConnected)
       continue;
     delete node._x_ignoreSelf;
@@ -412,7 +420,7 @@ var mergeProxyTrap = {
     if (name == Symbol.unscopables)
       return false;
     return objects.some(
-      (obj) => Object.prototype.hasOwnProperty.call(obj, name)
+      (obj) => Reflect.has(obj, name)
     );
   },
   get({ objects }, name, thisProxy) {
@@ -420,7 +428,7 @@ var mergeProxyTrap = {
       return collapseProxies;
     return Reflect.get(
       objects.find(
-        (obj) => Object.prototype.hasOwnProperty.call(obj, name)
+        (obj) => Reflect.has(obj, name)
       ) || {},
       name,
       thisProxy
@@ -428,7 +436,7 @@ var mergeProxyTrap = {
   },
   set({ objects }, name, value, thisProxy) {
     const target = objects.find(
-      (obj) => Object.prototype.hasOwnProperty.call(obj, name)
+      (obj) => Reflect.has(obj, name)
     ) || objects[objects.length - 1];
     const descriptor = Object.getOwnPropertyDescriptor(target, name);
     if (descriptor?.set && descriptor?.get)
@@ -450,6 +458,8 @@ function initInterceptors2(data2) {
   let recurse = (obj, basePath = "") => {
     Object.entries(Object.getOwnPropertyDescriptors(obj)).forEach(([key, { value, enumerable }]) => {
       if (enumerable === false || value === void 0)
+        return;
+      if (typeof value === "object" && value !== null && value.__v_skip)
         return;
       let path = basePath === "" ? key : `${basePath}.${key}`;
       if (typeof value === "object" && value !== null && value._x_interceptor) {
@@ -1618,7 +1628,7 @@ var Alpine = {
   get raw() {
     return raw;
   },
-  version: "3.13.5",
+  version: "3.13.6",
   flushAndStopDeferringMutations,
   dontAutoEvaluateFunctions,
   disableEffectScheduling,
@@ -2422,12 +2432,10 @@ magic("refs", (el) => {
 });
 function getArrayOfRefObject(el) {
   let refObjects = [];
-  let currentEl = el;
-  while (currentEl) {
-    if (currentEl._x_refs)
-      refObjects.push(currentEl._x_refs);
-    currentEl = currentEl.parentNode;
-  }
+  findClosest(el, (i) => {
+    if (i._x_refs)
+      refObjects.push(i._x_refs);
+  });
   return refObjects;
 }
 
@@ -3087,13 +3095,21 @@ function loop(el, iteratorNames, evaluateItems, evaluateKey) {
     if (isObject2(items)) {
       items = Object.entries(items).map(([key, value]) => {
         let scope2 = getIterationScopeVariables(iteratorNames, value, key, items);
-        evaluateKey((value2) => keys.push(value2), { scope: { index: key, ...scope2 } });
+        evaluateKey((value2) => {
+          if (keys.includes(value2))
+            warn("Duplicate key on x-for", el);
+          keys.push(value2);
+        }, { scope: { index: key, ...scope2 } });
         scopes.push(scope2);
       });
     } else {
       for (let i = 0; i < items.length; i++) {
         let scope2 = getIterationScopeVariables(iteratorNames, items[i], i, items);
-        evaluateKey((value) => keys.push(value), { scope: { index: i, ...scope2 } });
+        evaluateKey((value) => {
+          if (keys.includes(value))
+            warn("Duplicate key on x-for", el);
+          keys.push(value);
+        }, { scope: { index: i, ...scope2 } });
         scopes.push(scope2);
       }
     }
@@ -3141,7 +3157,7 @@ function loop(el, iteratorNames, evaluateItems, evaluateKey) {
       let marker = document.createElement("div");
       mutateDom(() => {
         if (!elForSpot)
-          warn(`x-for ":key" is undefined or invalid`, templateEl);
+          warn(`x-for ":key" is undefined or invalid`, templateEl, keyForSpot, lookup);
         elForSpot.after(marker);
         elInSpot.after(elForSpot);
         elForSpot._x_currentIfEl && elForSpot.after(elForSpot._x_currentIfEl);
@@ -3168,7 +3184,7 @@ function loop(el, iteratorNames, evaluateItems, evaluateKey) {
       };
       mutateDom(() => {
         lastEl.after(clone2);
-        initTree(clone2);
+        skipDuringClone(() => initTree(clone2))();
       });
       if (typeof key === "object") {
         warn("x-for key cannot be an object, it must be a string or an integer", templateEl);
@@ -3252,7 +3268,7 @@ directive("if", (el, { expression }, { effect: effect3, cleanup: cleanup2 }) => 
     addScopeToNode(clone2, {}, el);
     mutateDom(() => {
       el.after(clone2);
-      initTree(clone2);
+      skipDuringClone(() => initTree(clone2))();
     });
     el._x_currentIfEl = clone2;
     el._x_undoIf = () => {
